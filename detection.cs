9ce85cdf684061b05b0c10e20699049db11b05cd
Basker12 <71788109+Basker12@users.noreply.github.com>
2021-06-21 11:14:06 +0100

    [Addition]
    This adds hovers

---
+++ detection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detection : MonoBehaviour
+{
+    public float hoverForce = 12f;
+    
+    //Colliders
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("The Sphere Entered The Castle");
+        other.attachedRigidbody.AddForce(Vector3.up * hoverForce, ForceMode.Acceleration);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
